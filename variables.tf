430a6ff49d94cf133b0b63d752bbae30c3079d7f
Scott Collins <scott.collins@pollinate.co.uk>
2021-12-22 20:49:04 +0000

    added kubectl ingress creation for ag

--- variables.tf
+++ variables.tf
@@ -35,7 +35,9 @@ variable "location" {
     default = "uksouth"
 }
 
-variable "OBJECT_ID" {}
+variable "OBJECT_ID" {
+    default = "b9e6db17-8822-4bcc-aa78-957c26a9902e"
+}
 
 variable "CLIENT_ID" {}
 variable "CLIENT_SECRET" {}
